298effd6b1171475e4a4fb467809d99614e42d43
adrianjengel <adrian.engel@spsmail.cuny.edu>
2015-02-07 14:19:02 +0000

    Final upload of all modified files.

--- task_03.py
+++ task_03.py
@@ -1,7 +1,8 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Hi I am a docstring."""
+
+CHARLIE = "Brown"
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
-
-
-
