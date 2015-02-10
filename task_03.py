d3651592740c465e3b171d27f077566399f0c15a
modestocabrera <modesto.cabrera@spsmail.cuny.edu>
2015-02-10 06:21:42 +0000

    added docstring and corrected issues with file

--- task_03.py
+++ task_03.py
@@ -1,7 +1,8 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""A Color Scheme"""
+
+CHARLIE = "Chocolate Factory"
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
-
-
-
